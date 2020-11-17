49e424fc81b08f17b3c496d47e731bd229a16ea0
Ayaka Neko <neko@ayaka.moe>
2020-11-17 19:34:04 +0800

    :sparkles: 连接两个服务器的数据库
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,26 +2,51 @@ const mysql = require('mysql')
 const config = require('../../config/config')
 
 let cachePool = mysql.createPool(config.mysql.cache)
+let matatakiPool = mysql.createPool(config.mysql.matataki)
 
-let query = (sql, values) => {
-    return new Promise((resolve, reject) => {
-        cachePool.getConnection((err, connection) => {
-            if (err) {
-                reject(err)
-            } else {
-                connection.query(sql, values, (err, rows) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve(rows)
-                    }
-                    connection.release()
-                })
-            }
+let cache = {
+    query (sql, values) {
+        return new Promise((resolve, reject) => {
+            cachePool.getConnection((err, connection) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()
+                    })
+                }
+            })
         })
-    })
+    }
+}
+
+let matataki = {
+    query (sql, values) {
+        return new Promise((resolve, reject) => {
+            matatakiPool.getConnection((err, connection) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()
+                    })
+                }
+            })
+        })
+    }
 }
 
 module.exports = {
-    query
+    cache,
+    matataki
 }
