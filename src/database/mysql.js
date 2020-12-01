f42edf0beca4b861c44e9071b47fd99498eaf306
Ayaka Neko <neko@ayaka.moe>
2020-12-01 18:29:49 +0800

    :sparkles: 添加了测试网和正式网的数据库配置
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -3,6 +3,8 @@ const config = require('../../config/config')
 
 let cachePool = mysql.createPool(config.mysql.cache)
 let matatakiPool = mysql.createPool(config.mysql.matataki)
+let cacheTestPool = mysql.createPool(config.mysql.cacheTest)
+let matatakiTestPool = mysql.createPool(config.mysql.matatakiTest)
 
 let cache = {
     query (sql, values) {
@@ -46,7 +48,51 @@ let matataki = {
     }
 }
 
+let cacheTest = {
+    query (sql, values) {
+        return new Promise((resolve, reject) => {
+            cacheTestPool.getConnection((err, connection) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()
+                    })
+                }
+            })
+        })
+    }
+}
+
+let matatakiTest = {
+    query (sql, values) {
+        return new Promise((resolve, reject) => {
+            matatakiTestPool.getConnection((err, connection) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()
+                    })
+                }
+            })
+        })
+    }
+}
+
 module.exports = {
     cache,
-    matataki
+    matataki,
+    cacheTest,
+    matatakiTest
 }
