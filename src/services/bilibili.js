51d162ef72e61feacebbcf753e5adeffcfcb3ca6
Ayaka Neko <neko@ayaka.moe>
2020-12-01 18:30:56 +0800

    :sparkles: 添加了对应接口的数据库获取信息方法
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/services/bilibili.js
+++ src/services/bilibili.js
@@ -42,6 +42,41 @@ class BilibiliService {
         Log.debug('Bilibili 数据库返回的结果：' + JSON.stringify(res))
         return res
     }
+
+    static async addStatusListTest(statusList) {
+        let valuesSql = ``
+        const valuesData = []
+        for (let i = 0; i < statusList.length; i++) {
+            const { desc, card } = statusList[i]
+            const jsonData = JSON.stringify({ desc, card })
+            if (jsonData.length > 10239) {
+                Log.warning(`存入 ${PLATFORM} 动态时遇到过大的 Data JSON，id: ${desc.dynamic_id}`)
+                continue
+            }
+            valuesData.push(
+                PLATFORM + '_' + desc.dynamic_id,
+                PLATFORM,
+                desc.uid,
+                desc.uid,
+                desc.user_profile.info.uname,
+                moment(Math.round(Number(desc.timestamp) * 1000)).format('YYYY-MM-DD HH:mm:ss'),
+                jsonData
+            )
+            valuesSql += (valuesSql ? ',' : '') + '(?, ?, ?, ?, ?, ?, ?)'
+        }
+        if (!valuesSql) return null
+        const sql = `
+            INSERT INTO platform_status_cache
+                (id, platform, platform_user, platform_user_id, platform_username, timestamp, data)
+            VALUES
+                ${valuesSql}
+            ON DUPLICATE KEY UPDATE
+                data = VALUES(data);
+        `
+        const res = await Mysql.cacheTest.query(sql, valuesData)
+        Log.debug('Bilibili 数据库返回的结果：' + JSON.stringify(res))
+        return res
+    }
 }
 
 module.exports = BilibiliService
--- src/services/twitter.js
+++ src/services/twitter.js
@@ -44,6 +44,42 @@ class TwitterService {
         return res
     }
 
+    static async addStatusListTest(statusList) {
+        let valuesSql = ``
+        const valuesData = []
+        for (let i = 0; i < statusList.length; i++) {
+            const item = statusList[i]
+            const jsonData = JSON.stringify(item)
+            const date = moment(new Date(item.created_at), false).format('YYYY-MM-DD HH:mm:ss')
+            if (jsonData.length > 10239) {
+                Log.warning(`存入 ${PLATFORM} 动态时遇到过大的 Data JSON，id: ${item.id_str}`)
+                continue
+            }
+            valuesData.push(
+                PLATFORM + '_' + item.id_str,
+                PLATFORM,
+                item.user.screen_name,
+                item.user.id_str,
+                item.user.screen_name,
+                date,
+                jsonData
+            )
+            valuesSql += (valuesSql ? ',' : '') + '(?, ?, ?, ?, ?, ?, ?)'
+        }
+        if (!valuesSql) return null
+        const sql = `
+            INSERT INTO platform_status_cache
+                (id, platform, platform_user, platform_user_id, platform_username, timestamp, data)
+            VALUES
+                ${valuesSql}
+            ON DUPLICATE KEY UPDATE
+                data = VALUES(data);
+        `
+        const res = await Mysql.cacheTest.query(sql, valuesData)
+        Log.debug('Twitter 数据库返回的结果：' + JSON.stringify(res))
+        return res
+    }
+
     static async getUserList () {
         const sql = `
             SELECT
@@ -58,6 +94,23 @@ class TwitterService {
             `
 
         const res = await Mysql.matataki.query(sql)
+        return JSON.parse(JSON.stringify(res))
+    }
+
+    static async getUserListTest () {
+        const sql = `
+            SELECT
+                t1.*,
+                IFNULL(t2.timeline_switch, 0) as timeline_switch
+            FROM
+                user_accounts t1
+            LEFT JOIN
+                twitter_user_timeline_switch t2 ON t1.uid = t2.user_id
+            WHERE
+                t1.platform = 'twitter'
+            `
+
+        const res = await Mysql.matatakiTest.query(sql)
         return JSON.parse(JSON.stringify(res))
     }
 }
