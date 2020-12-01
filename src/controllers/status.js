162c6d64353a1b9a79b3a43bbf00a173ef6675e1
Ayaka Neko <neko@ayaka.moe>
2020-12-01 18:30:26 +0800

    :sparkles: 添加了接口对应的方法
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -1,4 +1,4 @@
-const TimelineService = require('../services/timeline')
+const { TimelineService, TimelineTestService } = require('../services/timeline')
 
 class StatusController {
     constructor() {}
@@ -12,7 +12,22 @@ class StatusController {
         console.log('请求的用户：', ctx.user)
         console.log('userId: ', ctx.user.id)
         const res = await TimelineService.getSubscribedTimeline(ctx.user.id, parseInt(page), parseInt(pagesize))
-        ctx.body = {
+        return {
+            code: 0,
+            data: res
+        }
+    }
+
+    static async getStatusTest (ctx) {
+        if (!ctx.user || !ctx.user.id) {
+            ctx.status = 403
+            return
+        }
+        const { page = 1, pagesize = 20 } = ctx.request.query
+        console.log('请求的用户：', ctx.user)
+        console.log('userId: ', ctx.user.id)
+        const res = await TimelineTestService.getSubscribedTimeline(ctx.user.id, parseInt(page), parseInt(pagesize))
+        return {
             code: 0,
             data: res
         }
