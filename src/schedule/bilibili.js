9824698d236c476b934ea7c3a1bc6b016eef284d
Ayaka Neko <neko@ayaka.moe>
2020-12-02 11:46:24 +0800

    :bug: Fixed wrong variable naming
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/schedule/bilibili.js
+++ src/schedule/bilibili.js
@@ -6,7 +6,7 @@ const Scheduler = require('./index')
 const bilibiliService = require('../services/bilibili')
 
 let getDynamicByID = async () => {
-    const list = await Axios.get(`https://auth.matataki.io/api/user/info/bilibili`, { params: { apiToken: config.apiKey } })
+    const list = await Axios.get(`https://auth.matataki.io/api/user/info/bilibili`, { params: { apiToken: config.apiToken } })
     try {
         list.data.forEach(async item => {
             const res = await Axios.get(`https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid=${item.userId}&offset_dynamic_id=0&need_top=1`)
@@ -19,7 +19,7 @@ let getDynamicByID = async () => {
 }
 
 let getDynamicByIDTest = async () => {
-    const list = await Axios.get(`https://auth.matataki.io/apitest/user/info/bilibili`, { params: { apiToken: config.apiKey } })
+    const list = await Axios.get(`https://auth.matataki.io/apitest/user/info/bilibili`, { params: { apiToken: config.apiToken } })
     try {
         list.data.forEach(async item => {
             if (!item.available) return
