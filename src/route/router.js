46a731d63e75fe7a14c225f28b7c35c0da978c42
Ayaka Neko <neko@ayaka.moe>
2020-12-22 20:48:45 +0800

    :sparkles: 应用路由表设置
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/route/router.js
+++ src/route/router.js
@@ -6,8 +6,12 @@ const app = require('../api/app')
 
 const status = require('../api/status')
 
+const sync = require('../api/sync')
+
 routers.use('/app', app.routes(), app.allowedMethods())
 
 routers.use('/status', status.routes(), status.allowedMethods())
+
+routers.use('/sync', sync.routes(), sync.allowedMethods())
 
 module.exports = routers
