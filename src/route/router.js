26219547c99fffcfef05e5ba286189ff86998cee
Ayaka Neko <neko@ayaka.moe>
2020-12-23 15:09:02 +0800

    :sparkles: 应用 report error 路由
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/route/router.js
+++ src/route/router.js
@@ -8,10 +8,14 @@ const status = require('../api/status')
 
 const sync = require('../api/sync')
 
+const report = require('../api/report')
+
 routers.use('/app', app.routes(), app.allowedMethods())
 
 routers.use('/status', status.routes(), status.allowedMethods())
 
 routers.use('/sync', sync.routes(), sync.allowedMethods())
+
+routers.use('/report', report.routes(), report.allowedMethods())
 
 module.exports = routers
